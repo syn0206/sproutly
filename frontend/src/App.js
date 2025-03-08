7e60b33c90dc2a3f8614a97108796c6ed05dd3e7
Yuna Shin <yoonahs@andrew.cmu.edu>
2025-03-08 18:11:15 +0900

    add app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavigationBar from './components/NavigationBar';
+import HomePage from './pages/HomePage';
+import MonitoringPage from './pages/MonitoringPage';
+import AddPlantPage from './pages/AddPlantPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <NavigationBar />
+        <div className="container mt-4">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/monitoring" element={<MonitoringPage />} />
+            <Route path="/add-plant" element={<AddPlantPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
+
 
 export default App;
